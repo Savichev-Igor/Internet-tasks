4e92ee969364781b9c0b793afd15dd95fea99db6
Puni <Ultimate95@mail.ru>
2015-03-28 22:39:27 +0500

    Now it workds

--- tracert (Savi).py
+++ tracert (Savi).py
@@ -11,7 +11,7 @@ import struct
 
 def createParser():
     parser = argparse.ArgumentParser(
-            prog='sudo python3 traceroute.py',
+            prog='python traceroute.py',
             description="""Эта программа аналог Unix Tracerout'a""",
             epilog='''(c) Puni ,2015. Автор программы, как всегда,
                        не несет никакой ответственности.'''
@@ -23,63 +23,10 @@ def createParser():
     return parser
 
 
-F1 = "BBHHH"
-F2 = "H"
-
-
-def calculate_checksum(source_string):
-    """
-    A port of the functionality of in_cksum() from ping.c
-    Ideally this would act on the string as a series of 16-bit ints (host
-    packed), but this works.
-    Network data is big-endian, hosts are typically little-endian
-    """
-    countTo = (int(len(source_string) / 2)) * 2
-    sum = 0
-    count = 0
-
-    # Handle bytes in pairs (decoding as short ints)
-    loByte = 0
-    hiByte = 0
-    while count < countTo:
-        if (sys.byteorder == "little"):
-            loByte = source_string[count]
-            hiByte = source_string[count + 1]
-        else:
-            loByte = source_string[count + 1]
-            hiByte = source_string[count]
-        sum = sum + (ord(hiByte) * 256 + ord(loByte))
-        count += 2
-
-    # Handle last byte if applicable (odd-number of bytes)
-    # Endianness should be irrelevant in this case
-    if countTo < len(source_string): # Check for odd length
-        loByte = source_string[len(source_string) - 1]
-        sum += ord(loByte)
-
-    sum &= 0xffffffff # Truncate sum to 32 bits (a variance from ping.c, which
-                      # uses signed ints, but overflow is unlikely in ping)
-
-    sum = (sum >> 16) + (sum & 0xffff)  # Add high 16 bits to low 16 bits
-    sum += (sum >> 16)                  # Add carry from above (if any)
-    answer = ~sum & 0xffff              # Invert and truncate to 16 bits
-    answer = socket.htons(answer)
-
-    return answer
-
-def checksum(icmp):  # Контрольная сумма
-    s = 0
-    for i in range(0, len(icmp), 2):
-        t = struct.unpack(F2, icmp[i:i+2])[0]+s
-        s = (t & 0xFFFF) + (t >> 16)
-    return ~s & 0xFFFF
-
-
-def request():  # ICMP REQUEST
-    p = struct.Struct(F1)
-    temp_packet = p.pack(8, 0, 0, 0, 0)
-    packet = struct.pack(F1, 8, 0, calculate_checksum(temp_packet), 0, 0)
-    return packet
+def take_packet():
+    packet = struct.Struct("BBHHH")
+    bin_packet = packet.pack(8, 0, 0xfff7, 0, 0)    # 65 527
+    return bin_packet
 
 
 class Traceroute:
@@ -89,29 +36,20 @@ class Traceroute:
         # self.dest_name = dest_name
         self.port = 33434
         self.max_hops = max_hops
-        # ICMP = socket.getprotobyname('icmp')
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        self.bin_packet = take_packet()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                               socket.IPPROTO_ICMP)
         self.s.settimeout(3.0)
-        # self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP)
-        # self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-        # self.recv_socket.settimeout(3.0)
-        # self.recv_socket.bind(("", self.port))
         self.regexp_iana = re.compile('whois:(.+)')
-        self.regexp_reg_AS = re.compile(':.+(as\d+)')
+        self.regexp_reg_AS = re.compile('origin:.+as(\d+)')
         self.regexp_reg_country = re.compile('country:(.+)')
         self.regexp_reg_provider = re.compile('descr:(.+)')
 
     def start(self):
-        # UDP = socket.getprotobyname('udp')
         ttl = 1
         while True:
-            # send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, UDP)
             self.s.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
-            self.s.sendto(request(), (self.dest_addr, self.port))
-            # send_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-            # send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-            # send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-            # send_socket.sendto(b'', (self.dest_addr, self.port))
+            self.s.sendto(self.bin_packet, (self.dest_addr, self.port))
             curr_addr = None
             curr_name = None
             try:
@@ -221,16 +159,16 @@ class Traceroute:
 
 if __name__ == "__main__":
     try:
-        if sys.platform == "win32":
-            os.system("netsh advfirewall firewall add rule name=ICMPV4 dir=in action=allow protocol=icmpv4 >nul")
-            print("Создано правило открытия портов для ICMPV4!")
         p = createParser()
         args = p.parse_args()
+        if sys.platform == "win32":
+            os.system("netsh advfirewall firewall add rule name=ICMPV4 \
+                       dir=in action=allow protocol=icmpv4 > nul")
         print("\nTraceroute for %s with %s hops...\n" % (args.destination,
                                                          args.Hops))
         A = Traceroute(args.destination, args.Hops)
         A.start()
     finally:
         if sys.platform == "win32":
-            os.system("netsh advfirewall firewall delete rule name=ICMPV4 dir=in protocol=icmpv4 >nul")
-            print("Удалено правило открытия портов для ICMPV4!")
+            os.system("netsh advfirewall firewall delete rule name=ICMPV4 \
+                       dir=in protocol=icmpv4 > nul")
