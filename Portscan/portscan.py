b5a09e7e2259d8361a2fb60fba3a4f78f283630a
Savi <Ultimate95@mail.ru>
2015-04-05 17:08:27 +0500

    Fixes and example

--- Portscan/portscan.py
+++ Portscan/portscan.py
@@ -1,7 +1,26 @@
+# !/usr/bin/python
 # -*- coding: utf-8 -*-
 import socket
 import threading
+import argparse
 # import time
+
+
+def createParser():
+    parser = argparse.ArgumentParser(
+            prog='python portscan.py',
+            description="""Эта программа сканирует TCP/UDP порты.
+                        """,
+            epilog='''(c) Puni, 2015. Автор программы, как всегда,
+                      не несет никакой ответственности.
+                   '''
+            )
+    parser.add_argument("HOST", type=str,
+                        help="IP адрес удаленного компьютера")
+    parser.add_argument("PORTS", type=str,
+                        help="Диапазон портов")
+    return parser
+
 
 # ports_TCP = []
 
@@ -22,8 +41,8 @@ class Scanner_TCP(threading.Thread):
             self.s.connect((self.ip_addr, self.port))
             try:
                 print('TCP: {0} {1} {2}'.format(self.ip_addr,
-                                                  self.port,
-                                                  socket.getservbyport(self.port)))
+                                                self.port,
+                                                socket.getservbyport(self.port)))
             except socket.error as msg_2:
                 print('TCP: {0} {1}'.format(self.ip_addr, self.port))
             # global ports_TCP
@@ -47,21 +66,33 @@ class Scanner_UDP(threading.Thread):
         self.s.settimeout(0.1)
 
     def run(self):
-        # try:
+        try:
             self.s.sendto(b"LOL", (self.ip_addr, self.port))
-            print(self.s.recv(1024))
-            # try:
-            #     print('UDP: {0} {1} {2}'.format(self.ip_addr,
-            #                                     self.port,
-            #                                     socket.getservbyport(self.port)))
-            # except socket.error as msg_2:
-            #     print('UDP: {0} {1}'.format(self.ip_addr, self.port))
+            try:
+                self.s.recv(1024)
+                try:
+                    print('UDP: {0} {1} {2}'.format(self.ip_addr,
+                                                    self.port,
+                                                    socket.getservbyport(self.port)))
+                except socket.error as msg_3:
+                    print('UDP: {0} {1}'.format(self.ip_addr, self.port))
+            except socket.error as ms_2:
+                pass
             # global ports_UDP
             # ports_UDP.append(self.port)
             self.s.close()
-        # except socket.error as msg_1:
-            # pass
+        except socket.error as msg_1:
+            pass
 
-for port in range(65536):  # All ports and + 1
-    # Scanner_TCP('127.0.0.1', port).start()
-    Scanner_UDP('127.0.0.1', port).start()
+
+def main():
+    p = createParser()
+    args = p.parse_args()
+    PORTS = args.PORTS.split('-')
+    print()
+    for port in range(int(PORTS[0]), int(PORTS[1])+1):  # All ports and + 1
+        Scanner_TCP(args.HOST, port).start()
+        Scanner_UDP(args.HOST, port).start()
+
+if __name__ == "__main__":
+    main()
